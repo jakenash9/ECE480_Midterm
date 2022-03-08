5f12341d953a4c9af543fe70b476295312449fc0
Jake Nash <nashjc@rams.colostate.edu>
2022-03-08 13:46:23 -0700

    uh

--- sat.py
+++ sat.py
@@ -1,13 +1,39 @@
 import sys
 
+from sqlalchemy import false
+
 def main():
-   with open('cnf.cnf') as f:
-    cnf = f.readlines()
-    # print(cnf)
-    output_var(cnf)
-    cnf_small = cnf[2:]
-    unit_clause(cnf_small)
-    print(cnf_small[0])
+    inFile = open("cnf.cnf", "r")
+    allLines = inFile.readlines()
+    clauses = 0
+    variables = 0
+    cnf = []
+    global true, false
+    true = []
+    false = []
+    for x in allLines:
+        if x[0]=='c':
+            continue
+        if x[0]=="p":
+            temp = x.split()
+            variables = int(temp[2])
+            clauses = int(temp[3])
+        else:
+            temp = x[:len(x)-2].split()
+            # print(temp)
+            for t in range(len(temp)):
+                temp[t]=int(temp[t])
+            cnf.append(temp)
+    # print(clauses, variables)
+    print(cnf)
+    dpll(cnf)
+#    with open('cnf.cnf') as f:
+#     cnf = f.readlines()
+#     # print(cnf)
+#     output_var(cnf)
+#     cnf_small = cnf[2:]
+#     unit_clause(cnf_small)
+#     print(cnf_small[0])
 
     
 # define the output of the cnf
@@ -17,11 +43,19 @@ def output_var(cnf):
     print(out)
     return out
 
-def unit_clause(cnf_small):
-    for x in cnf_small: # each line
-        for k in x: # each number
-            print("idk")
-            
+def unit_clause(cnf):
+    units = []
+    for x in cnf: # each line
+        if len(x) == 1:
+            units.append(x[0])
+    print(units)
+    
+    return cnf
+
+def dpll(cnf):
+    cnf = unit_clause(cnf)
+    # print(cnf)
+
 
 
     
