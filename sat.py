cb8bc313150053791b2f7064ad6eb5fc7b5fbc80
Jake Nash <nashjc@rams.colostate.edu>
2022-03-08 18:18:07 -0700

    unit_clause comments done

--- sat.py
+++ sat.py
@@ -29,41 +29,52 @@ def main():
             cnf.append(temp)
 
     print(cnf) # print cnf array
+    print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
 
     dpll(cnf) # call dpll algorithm
 
-
+# unit propagation clause - based on sudo code from DPLL Wikipedia
 def unit_clause(cnf):
+    # define arrays
     units = []
     pos = []
     neg = []
-    for x in cnf: # each line
+    for x in cnf: # each line in cnf.cnf
+        # check if it is a unit clause and if so, add it to units array
         if len(x) == 1:
             units.append(x[0])
-    for u in units:
+    for u in units: # each unit clause (standalone literal)
+        # if unit literal is negative, add to neg and false arrays
         if u < 0: 
             neg.append(u)
             false.append(u)
+        # if unit literal is positive, add to pos and true arrays
         else: 
             pos.append(u)
             true.append(u)
-        cnf = [x for x in cnf if u not in x] # remove satisfied clauses from array
-        # remove unsat literal from clauses
+        cnf = [x for x in cnf if u not in x] # remove SAT clauses from cnf array
+        print(cnf, "doodooshat")
+        # remove UNSAT literal from clauses in cnf array
         for x in cnf:
             if -u in x:
                 x.remove(-u)
-    return cnf, pos, neg
+    return cnf, pos, neg # return cnf array, pos unit literal array, neg unit literal array
 
-def pure(cnf, pos, neg): # check to see if a literal appears as ALWAYS positive or ALWAYS negative
-    positive = []
-    negative = []
-    for x in cnf:
-        for q in x:
+# pure literal elimination method - based on sudo code from DPLL Wikipedia
+# checks to see if a literal appears as ALWAYS positive or ALWAYS negative
+def pure(cnf, pos, neg):
+    # define positive and negative arrays after unit clauses removed
+    positive = [] # positive literals 
+    negative = [] # negative literals
+    for x in cnf: # each clause in cnf after disgarded unit clauses
+        for q in x: # sort each literal into positive and negative arrayy
             if q > 0: positive.append(q)
             if q < 0: negative.append(q)
-    pOnly = []
-    nOnly = []
-    for x in positive:
+    # define pOnly and nOnly arrays
+    pOnly = [] # literals that only appear positive
+    nOnly = []  # literals that only appear negative
+    for x in positive: # each positive literal
+        # if a positive literal never appears negative, set to true and add to pOnly array
         if -x not in negative and x not in pOnly:
             true.append(x)
             pOnly.append(x)
