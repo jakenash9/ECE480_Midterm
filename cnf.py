ed0a22894bf2aef5fe28b57947b44338c8772b60
Jake Nash <nashjc@rams.colostate.edu>
2022-03-11 16:17:50 -0700

    count

--- cnf.py
+++ cnf.py
@@ -1,4 +1,4 @@
-from re import X
+
 import sys
 
 def main():
@@ -8,7 +8,7 @@ def main():
     print(f_or)
     print("------------------------")
     lits_array = [] # create new array for literals
-    lits = []
+    counter, lit = var_count(x)
 
 
     # for each clause in f_or split at AND gates and append to lits
@@ -29,7 +29,29 @@ def main():
 
 
 
-
+def var_count(f):
+    ors = f.split("+") # split by OR gates
+    lits = [] # array of literals
+    count = -1 # count for highest literal
+    # for each AND clause
+    for x in ors:
+        ands = x.split(".") # split into literals
+        # for each literal
+        for i in ands:
+            # if literal has a NOT, remove it (don't need negative for counting)
+            if i.find("~") != -1:
+                i = i[1:]
+            # if literal is not already in lits array, add it
+            if i not in lits:
+                lits.append(i)
+    # for each literal number 
+    for j in lits:
+        numLit = int(j[1:])
+        # if current literal number is higher than count, make count = current literal
+        if numLit > count:
+            count = numLit
+    
+    return count+1, lits # return highest count+1 for new output, and lits array
 
 
   
